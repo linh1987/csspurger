9dbc28af8ef26d6ac5a460e034786396864358c3
Bang Cao Doan <bang.doan@niteco.se>
2016-07-29 14:11:19 +0700

    Update crawler

--- main.js
+++ main.js
@@ -15,19 +15,37 @@
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: false });
 var http = require('http');
+var request = require('request');
 
 nightmare
-  .goto('http://www.niteco.se')
-  .evaluate(function () {
+    .goto('http://www.niteco.se')
+    .evaluate(function () {
       var linkStylesheets = document.querySelectorAll('link[rel=stylesheet]');
       var externCss = Array.prototype.map.call(linkStylesheets, function(link) { return link.href; });
       return externCss;
-  })
-  .end()
-  .then(function (result) {
-    
-  })
-  .catch(function (error) {
+    })
+    .end()
+    .then(function (links) {
+        return new Promise(function(resolve, reject) {
+            var cssData = [];
+            
+            for(var i=0; i < links.length; i++){
+                request(links[0], function (error, response, body) {
+                    if (!error && response.statusCode == 200){
+                        cssData.push(body);
+                    }
+                    
+                    if (cssData.length === links.length) {
+                        resolve(cssData);
+                    }
+                })
+            }
+        });
+    })
+    .then(function(cssFiles){
+        console.log(cssFiles);
+    })
+    .catch(function (error) {
     console.error('Search failed:', error);
-  })
-  ;
+    })
+    ;
